a9dfb0283ac3f384a4b15d8bd6a65fcecd8657a8
Stan Wiechers <whoisstan@gmail.com>
2013-09-20 12:07:49 -0400

    removing ruby decl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 AssetTest::Application.configure do
+    config.force_ssl = true  
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
